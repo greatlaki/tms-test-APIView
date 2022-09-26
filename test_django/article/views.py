f4d9a423ebaa79950d795bcc8d41edb9ac99feb8
greatlaki <greatlaki911@gmail.com>
2022-09-26 23:54:44 +0300

    add serializer to view

--- test_django/article/views.py
+++ test_django/article/views.py
@@ -1,9 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from .models import Article
+from .serializers import ArticleSerializer
 
 
 class ArticleView(APIView):
     def get(self, request):
         articles = Article.objects.all()
-        return Response({"articles": articles})
+        # the many param informs the serializer that it will be serializing more than a single article.
+        serializer = ArticleSerializer(articles, many=True)
+        return Response({"articles": serializer.data})
