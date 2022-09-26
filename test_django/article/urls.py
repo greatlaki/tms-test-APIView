6b19fb39fad58af36b2f20653e1d19d81189d967
greatlaki <greatlaki911@gmail.com>
2022-09-27 00:19:56 +0300

    put request

--- test_django/article/urls.py
+++ test_django/article/urls.py
@@ -6,4 +6,5 @@ app_name = "articles"
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
     path('articles/', ArticleView.as_view()),
+    path('articles/<int:pk>', ArticleView.as_view())
 ]
--- test_django/article/views.py
+++ test_django/article/views.py
@@ -1,3 +1,4 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -19,3 +20,13 @@ class ArticleView(APIView):
         if serializer.is_valid(raise_exception=True):
             article_saved = serializer.save()
         return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
+
+    def put(self, request, pk):
+        saved_article = get_object_or_404(Article.objects.all(), pk=pk)
+        data = request.data.get('articles')
+        serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+        return Response({
+            "success": "Article '{}' updated successfully".format(article_saved.title)
+        })
