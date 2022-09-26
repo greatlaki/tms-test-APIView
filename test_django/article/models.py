495096fc7b1d32135b24268c9fdffc01a06d28d0
greatlaki <greatlaki911@gmail.com>
2022-09-26 23:46:31 +0300

    add displaying the model

--- test_django/article/models.py
+++ test_django/article/models.py
@@ -5,6 +5,9 @@ class Author(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
 
+    def __str__(self):
+        return self.title
+
 
 class Article(models.Model):
     title = models.CharField(max_length=120)
