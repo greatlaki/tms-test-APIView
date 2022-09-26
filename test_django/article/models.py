ac489623a949df2bb4e6809c38508e5b486d41d0
greatlaki <greatlaki911@gmail.com>
2022-09-26 23:36:54 +0300

    add displaying the model

--- test_django/article/models.py
+++ test_django/article/models.py
@@ -11,3 +11,6 @@ class Article(models.Model):
     description = models.TextField()
     body = models.TextField()
     author = models.ForeignKey('Author', related_name='articles', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
