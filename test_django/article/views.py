9f94aef240b84f0062755bbff2ec5522a8175223
greatlaki <greatlaki911@gmail.com>
2022-09-27 00:22:18 +0300

    article delete

--- test_django/article/views.py
+++ test_django/article/views.py
@@ -30,3 +30,11 @@ class ArticleView(APIView):
         return Response({
             "success": "Article '{}' updated successfully".format(article_saved.title)
         })
+
+    def delete(self, request, pk):
+        # Get object with this pk
+        article = get_object_or_404(Article.objects.all(), pk=pk)
+        article.delete()
+        return Response({
+            "message": "Article with id `{}` has been deleted.".format(pk)
+        }, status=204)
